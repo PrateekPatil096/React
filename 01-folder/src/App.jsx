020d225adc00c99d4f2d3089932dfd2f5d635b98
Prateek Patil <prateekpatil096@gmail.com>
2026-08-10 13:36:23 +0530

    react

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div id='dad'>
+      <h1 id='child1'>hello</h1>
+      <h2 id='child2'>hello</h2>
+      
+    </div>
+    <div id="chaca">
+      </div>
+      </div>
+  )
+}
+
+export default App

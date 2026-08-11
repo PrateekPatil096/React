7ecc37b9f551ea8d9dd94d143875ce9d6a8a1068
Prateek Patil <prateekpatil096@gmail.com>
2026-08-11 11:40:00 +0530

    components in react

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+            <h1>gokku</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit ut facilis necessitatibus similique, eius, accusantium, laborum minima neque amet debitis ea sunt? Corrupti accusamus ipsam molestias unde, ullam ex nemo!</p>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Card
